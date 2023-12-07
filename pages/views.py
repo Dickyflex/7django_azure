798028a4a737d8ecf2a2c2bf3085ad72dfdee1c3
AJ <ankit@primecarecorp.com>
2023-12-07 11:45:30 -0500

    modified message

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home_page_view(request):
-    return HttpResponse("Hello World! from CEI 202310 Cohort.")
+    return HttpResponse("Hello World! from CEI 202310 Cohort. Another Sentence.")
